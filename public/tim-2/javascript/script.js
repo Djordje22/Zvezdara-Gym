ebc3a53f59028e5525d6484975a0e0392f8a4540
Djordje22 <djordje.bojovic.22@gmail.com>
2021-11-01 12:13:44 +0100

    First commit

---
+++ public/tim-2/javascript/script.js
@@ -0,0 +1,58 @@
+function change(){
+		if(document.getElementById("changeB").style.display = "none"){
+			document.getElementById("show").innerHTML = "Kontakt telefon: 065/28 33 289";
+		}
+	}
+function change2(){
+		if(document.getElementById("changeB2").style.display = "none"){
+			document.getElementById("show2").innerHTML = "<a>Email:info@gym.rs</a>";
+		}
+	}
+function f(){
+		var x = document.getElementById("x").value;
+		if(x.length == 0){
+			document.getElementById("x").style.backgroundColor = "green";
+		}
+}
+function b(){
+		var x = document.getElementById("x").value;
+		if(x.length == 0){
+			document.getElementById("x").style.backgroundColor = "";
+		}
+}
+function f2(){
+		var x = document.getElementById("x2").value;
+		if(x.length == 0){
+			document.getElementById("x2").style.backgroundColor = "green";
+		}
+}
+function b2(){
+		var x = document.getElementById("x2").value;
+		if(x.length == 0){
+			document.getElementById("x2").style.backgroundColor = "";
+		}
+}
+function f3(){
+		var x = document.getElementById("x3").value;
+		if(x.length == 0){
+			document.getElementById("x3").style.backgroundColor = "green";
+		}
+}
+function b3(){
+		var x = document.getElementById("x3").value;
+		if(x.length == 0){
+			document.getElementById("x3").style.backgroundColor = "";
+		}
+}
+function f4(){
+		var x = document.getElementById("x4").value;
+		if(x.length == 0){
+			document.getElementById("x4").style.backgroundColor = "green";
+		}
+}
+function b4(){
+		var x = document.getElementById("x4").value;
+		if(x.length == 0){
+			document.getElementById("x4").style.backgroundColor = "";
+		}
+}
